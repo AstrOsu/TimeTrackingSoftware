53c5de3e16560cff407dd173fdb897f1e18187af
Andrew Core <andrew.core@computrols.com>
2018-02-16 19:51:49 -0600

    Created main.java, implemented the timetracker package

---
+++ timetracker/Main.java
@@ -0,0 +1,10 @@
+package timetracker;
+
+public class Main
+{
+
+    public static void main(String[] args)
+    {
+	// write your code here
+    }
+}
--- timetracker/blockStorage.java
+++ timetracker/blockStorage.java
@@ -8,6 +8,7 @@ import java.util.HashMap;
  *
  * @author GG46195x
  */
-public class blockStorage {
+public class blockStorage
+{
     HashMap<Date, String> timeBlocks = new HashMap<>(); //key is date and stored by description -- still need to learn hashMaps
 }
