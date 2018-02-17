1add02e61ff173f32e826ab4867fd66d87812ed8
Unknown <gengorman@gmail.com>
2018-02-16 18:25:29 -0600

    adds blockStorage class

---
+++ blockStorage.java
@@ -0,0 +1,13 @@
+
+package timetracker;
+
+import java.util.Date;
+import java.util.HashMap;
+
+/**
+ *
+ * @author GG46195x
+ */
+public class blockStorage {
+    HashMap<Date, String> timeBlocks = new HashMap<>(); //key is date and stored by description -- still need to learn hashMaps
+}
