d6cef660d277d73c7a31956e16b2cc68f6c23d23
Andrew Core <andrew.core@computrols.com>
2018-04-22 19:02:32 -0500

    Added minor comments

--- timetracker/Main.java
+++ timetracker/Main.java
@@ -15,6 +15,7 @@ public class Main
      * @param args the command line arguments
      */
     public static void main(String args[]) throws InterruptedException, IOException {
+        //Create and populate blockStorage
         blockStorage b = new blockStorage();
         //timeBlock tb = new timeBlock("" + System.currentTimeMillis());
         b.importFile("TimeBlocks.txt");
@@ -24,6 +25,7 @@ public class Main
         String[] hello = new String[5];
         HomePage.main(hello);
 
+        //Save Hashmap to a file
         FileOutputStream outputStream = new FileOutputStream("storage.txt");
         byte[] strToBytes = b.toString().getBytes();
         outputStream.write(strToBytes);
